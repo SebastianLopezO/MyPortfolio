40c479528b09099e011ba386a6d1a92c724cab67
Sebastián López O <sebastianlopezosorno2005@gmail.com>
2026-02-06 09:45:43 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import {componentTagger} from "lovable-tagger";
-import injectPreload from 'vite-plugin-inject-preload';
+//import injectPreload from 'vite-plugin-inject-preload';
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => ({
