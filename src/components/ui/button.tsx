b31433a5ef143c1a16317f03308df7f129bb72ea
Sebastián López O <sebastianlopezosorno2005@gmail.com>
2026-02-05 22:04:59 -0500

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,8 +4,6 @@ import {type VariantProps} from "class-variance-authority";
 import {cn} from "@/lib/utils";
 import {buttonVariants} from "@/components/ui/button-variants";
 
-export {buttonVariants} from "@/components/ui/button-variants";
-
 export interface ButtonProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement>,
         VariantProps<typeof buttonVariants> {
