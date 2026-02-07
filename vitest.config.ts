cd52b47447e12a26177eaad0c96864c18f0c46c4
Sebastián López O <sebastianlopezosorno2005@gmail.com>
2026-02-07 11:49:07 -0500

    resolver lint conflict, with TS1259

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 import {defineConfig} from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { resolve } from "path";
 
 export default defineConfig({
     plugins: [react()],
@@ -17,7 +17,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            "@": path.resolve(__dirname, "./src"),
+            "@": resolve(__dirname, "./src"),
         },
     },
 });
